29788f99ce9faa1dd8b233eb89b283f3d3b6ae77
muhammedNafis <nafisur257@gmail.com>
2019-11-16 19:20:07 +0600

    updated

--- myApp/src/screens/DetailsScreen.js
+++ myApp/src/screens/DetailsScreen.js
@@ -11,17 +11,17 @@ export default class HomeScreen extends Component{
         const item = navigation.getParam('item',{});
         //console.log('item in details screen',item);
         return(
-                <View style={{flex:1,padding:17,backgroundColor:'orange'}}>
+                <View style={{flex:1,padding:10,backgroundColor:'orange'}}>
                      <Image
-                        style={{height:200,flex:1}} 
+                        style={{height:200,flex:1,borderRadius:7}} 
                         resizeMode="cover" 
                         source={{uri:item.image_url}}
                     />
                     <Text style={{fontWeight:'bold',fontSize:17,padding:10}}>Recipe name : {item.title}</Text>
-                    <Text style={{fontSize:17,padding:10}}>This recipe is published by {item.publisher}</Text>
+                    <Text style={{fontSize:17,padding:9}}>This recipe is published by {item.publisher}</Text>
                     <Text style={{color:'blue'}}>   {item.publisher_url}.</Text>
-                    <Text style={{fontSize:17,padding:10}}>Its current social rank is {item.social_rank}.</Text>
-                    <Text style={{fontSize:17,padding:10}}>Full recipe details is in the given link :</Text>
+                    <Text style={{fontSize:17,padding:9}}>Its current social rank is {item.social_rank}.</Text>
+                    <Text style={{fontSize:17,padding:9}}>Full recipe details is in the given link :</Text>
                     <Text style={{color:'blue'}}>   {item.source_url}</Text>
                 </View>
             
